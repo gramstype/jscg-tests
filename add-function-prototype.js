c5a7bbdc1dd25fc1914964c24431c9561b0b7a4a
gramstype <gramstype@gmail.com>
2022-07-08 12:52:46 -0500

    edited comments

--- add-function-prototype.js
+++ add-function-prototype.js
@@ -1,10 +1,10 @@
+// Credits: https://teamtreehouse.com/community/adding-methods-to-javascript-prototypes
 function A () {
     this.f = function () {
         console.log('A.this.f');
     }
 }
 
-//second method uses constructor.prototype.methodName
 A.prototype.g = function () {
     console.log('A.prototype.g');
 };
